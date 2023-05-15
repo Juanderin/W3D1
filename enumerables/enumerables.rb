cc653ad44ca47f07cadd2653f9fe72c7fbcde796
Theofanis Neofotistos <neofotistostheofanis@gmail.com>
2023-05-15 15:30:43 -0400

    my_select

--- enumerables/enumerables.rb
+++ enumerables/enumerables.rb
@@ -1,6 +1,6 @@
 class Array 
 
-    def self.my_each(&prc) 
+    def my_each(&prc) 
 
         i = 0 
         while i < self.length
@@ -12,14 +12,35 @@ class Array
 
     end 
 
+    def my_select(&prc)
+        new_array = []
+
+        i = 0 
+        while i < self.length 
+           if prc.call(self[i])
+            new_array << self[i] 
+           end   
+           i += 1
+        end
+        new_array
+    end
+    
+
+
+
 
 end 
 
 
-return_value = [1, 2, 3].my_each do |num|
-    puts num
-   end.my_each do |num|
-    puts num
-   end
+# return_value = [1, 2, 3].my_each do |num|
+#     puts num
+#    end.my_each do |num|
+#     puts num
+#    end
 
-p return_value  # => [1, 2, 3]
+# p return_value  # => [1, 2, 3]
+
+
+p a = [1, 2, 3]
+p a.my_select { |num| num > 1 } # => [2, 3]
+p a.my_select { |num| num == 4 } # => []
